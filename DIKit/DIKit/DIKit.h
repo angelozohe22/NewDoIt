8856d17fa8efefa1e2f7fae28649395bf6a613b9
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-01-26 16:20:30 -0500

    [Feature][DIKit] Create framework DIKit

---
+++ DIKit/DIKit/DIKit.h
@@ -0,0 +1,18 @@
+//
+//  DIKit.h
+//  DIKit
+//
+//  Created by Luis Angel Inga Mendoza on 26/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DIKit.
+FOUNDATION_EXPORT double DIKitVersionNumber;
+
+//! Project version string for DIKit.
+FOUNDATION_EXPORT const unsigned char DIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIKit/PublicHeader.h>
+
+
